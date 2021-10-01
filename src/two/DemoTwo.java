bcbe60c03370e098b1b6a0f49b4764b5d369441f
ABNER <ABNER@ABNER.lan>
2021-09-30 21:06:48 -0400

    added condition of xyz user story

--- src/two/DemoTwo.java
+++ src/two/DemoTwo.java
@@ -3,6 +3,15 @@ package two;
 public class DemoTwo {
 	public static void main(String[] args) {
 		System.out.println("Demo Two...");
+		
+		System.out.println("Second commit...");
+		
+		boolean isSunday = true;
+		if (isSunday) {
+			System.out.println("We won..");
+		} else {
+			System.out.println("yep..");
+		}
 	}
 
 }
